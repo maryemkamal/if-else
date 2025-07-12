ee8d504b5a75c6d9b5a7bb7a32a9bf4aeb3c679b
maryemkamal <121868686+maryemkamal@users.noreply.github.com>
2025-07-12 21:18:39 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+
+using namespace std;
+
+
+//problem 1
+/*
+int main()
+{   int num1 , num2 ;
+
+    cout << "plz enter two integer :" << endl;
+    cin>> num1 >>num2 ;
+
+    if(num1 == num2 )
+        cout<<"equal"<<endl;
+    else
+        cout<<"not equal"<<endl;
+    return 0;
+}
+*/
+
+//problem 2
+/*
+int main()
+{
+    int num ;
+    cout << "enter number to check is odd or even :" << endl;
+    cin >>num ;
+    if(num % 2==0)
+        cout<<"even"<<endl;
+    else
+        cout<<"odd"<<endl;
+    return 0;
+}
+*/
+
+//problem 3
+/*
+int main()
+{
+    int num ;
+    cout << "enter number to check is positive or negative or equal zero  :" << endl;
+    cin >>num ;
+    if(num > 0)
+        cout<<"positive"<<endl;
+    else if(num < 0)
+        cout<<"negative"<<endl;
+    else
+        cout<<"equal zero"<<endl;
+
+    return 0;
+}
+*/
+
+//problem 4
+/*
+int main()
+{
+    int day ;
+    cout << "enter day number  :" << endl;
+    cin >>day;
+
+    if(day == 1)
+        cout<<"sunday"<<endl;
+    else if(day == 2)
+        cout<<"monday"<<endl;
+    else if(day == 3)
+        cout<<"tuesday"<<endl;
+    else if(day == 4)
+        cout<<"wednesday"<<endl;
+    else if(day == 5)
+        cout<<"thursday"<<endl;
+    else if(day == 6)
+        cout<<"friday"<<endl;
+    else if(day == 7)
+        cout<<"saturday"<<endl;
+
+    else
+        cout<<"invalid day number , plz enter number 1:7"<<endl;
+       return 0;
+}
+
+*/
+//problem 5
+/*
+int main()
+{
+    int num1 , num2 ;
+
+    cout << "plz enter two integer :" << endl;
+    cin>> num1 >>num2 ;
+
+    if(num1 > num2)
+        cout<<"the max number is "<<num1;
+    else
+        cout<<"the max number is "<<num2;
+    return 0;
+}
+*/
